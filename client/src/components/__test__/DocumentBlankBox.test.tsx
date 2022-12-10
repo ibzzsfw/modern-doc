18efe122686cf9736cb9484516f894df60c128d4
Freewifi-100 <peeraya.4869@gmail.com>
2022-12-11 00:26:03 +0700

    `feat`: New breadcrumb

--- client/src/components/__test__/DocumentBlankBox.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import DocumentBlankBox from '@components/DocumentBlankBox'
-
-describe('DocumentBlankBox', () => {
-    it('should render', () => {
-      const { container } = render(<DocumentBlankBox />)
-      expect(container).toBeInTheDocument()
-    })
-})
