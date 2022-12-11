045e88c555d37faf087525ce9bb95c1139895c3e
Ball <ball.fiy555@gmail.com>
2022-12-11 20:18:25 +0700

    `feat` : api Free note

--- client/src/models/NoteController.tsx
+++ client/src/models/NoteController.tsx
@@ -1,6 +1,5 @@
-import Note from '@models/Note'
-import axios from 'axios'
 import { useLoginDataStore } from '@stores/LoginDataStore'
+import axios from 'axios'
 
 class NoteController {
   static async getLastestNote() {
@@ -31,9 +30,14 @@ class NoteController {
     return response.data
   }
 
-  static async addFreeNote() {
+  static async addFreeNote(heading: string, content: string) {
+    console.log(heading, content)
     let response = await axios.post(
       `${import.meta.env.VITE_API_ENDPOINT}/note/add`,
+      {
+        heading: heading,
+        content: content,
+      },
       {
         headers: {
           'user-id': useLoginDataStore.getState()?.user?.id,
@@ -41,6 +45,26 @@ class NoteController {
         },
       }
     )
+    window.location.reload()
+    return response.data
+  }
+
+  static async editNote(heding: string, content: string, id: string | undefined) {
+    console.log(heding, content, id)
+    let response = await axios.put(
+      `${import.meta.env.VITE_API_ENDPOINT}/note/edit/${id}`,
+      {
+        heading: heding,
+        content: content,
+      },
+      {
+        headers: {
+          'user-id': useLoginDataStore.getState()?.user?.id,
+          token: useLoginDataStore.getState()?.user?.token,
+        }
+      }
+    )
+
     return response.data
   }
 }
