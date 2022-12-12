76d3b10859eb0aa2f02d50b4c2b000422df91c53
Freewifi-100 <peeraya.4869@gmail.com>
2022-12-12 16:59:23 +0700

    `feat` : DocumentBlankBox test

--- client/src/components/__test__/DocumentBlankBox.test.tsx
+++ client/src/components/__test__/DocumentBlankBox.test.tsx
@@ -2,14 +2,14 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import DocumentBlankBox from '@components/DocumentBlankBox'
 
 describe('DocumentBlankBox', () => {
-    it('should render', () => {
-      const { container } = render(<DocumentBlankBox />)
-      expect(container).toBeInTheDocument()
-    })
-    it('should call onClick', () => {
-      const onClick = jest.fn()
-      render(<DocumentBlankBox onClick={onClick} />)
-      fireEvent.click(screen.getByRole('group'))
-      expect(onClick).toHaveBeenCalled()
-    })
+  it('should render', () => {
+    const { container } = render(<DocumentBlankBox />)
+    expect(container).toBeInTheDocument()
+  })
+  it('should call onClick', () => {
+    const onClick = jest.fn()
+    render(<DocumentBlankBox onClick={onClick} />)
+    fireEvent.click(screen.getByRole('group'))
+    expect(onClick).toHaveBeenCalled()
+  })
 })
