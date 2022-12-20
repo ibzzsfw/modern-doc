524cafbcd32263456fa0c9b24c578075445c2503
Suppakorn Rakna <68814521+ibzzsfw@users.noreply.github.com>
2022-12-20 21:39:26 +0700

    `fix` : Jate do not like it

--- client/src/firebase/index.ts
+++ client/src/firebase/index.ts
@@ -14,12 +14,12 @@ import {
 } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVYNSOxJmmfCmtx1Sd7FLy4Rwxx7Jk3ps',
-  authDomain: 'moderndoc-6ea6d.firebaseapp.com',
-  projectId: 'moderndoc-6ea6d',
-  storageBucket: 'moderndoc-6ea6d.appspot.com',
-  messagingSenderId: '593768057754',
-  appId: '1:593768057754:web:108af48568641215128823',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 // Initialize Firebase
